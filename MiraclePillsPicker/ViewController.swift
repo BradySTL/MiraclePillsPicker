56285182fc2728132f5e00dd2c36aca8d9f2b3e2
Brady Mirkes <bradymirkes@gmail.com>
2016-09-21 18:55:33 -0500

    Added State Button Label update

--- MiraclePillsPicker/ViewController.swift
+++ MiraclePillsPicker/ViewController.swift
@@ -82,6 +82,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
 
     @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -92,6 +93,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
     }
 }
 
