7641688d3482ba986c280981afd5d9d53bb0f7e6
Brady Mirkes <bradymirkes@gmail.com>
2016-09-21 18:40:39 -0500

    Added Protocols and States Array

--- MiraclePillsPicker/ViewController.swift
+++ MiraclePillsPicker/ViewController.swift
@@ -8,10 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = [ "AK - Alaska",
+                   "AL - Alabama",
+                   "AR - Arkansas",
+                   "AS - American Samoa",
+                   "AZ - Arizona",
+                   "CA - California",
+                   "CO - Colorado",
+                   "CT - Connecticut",
+                   "DC - District of Columbia",
+                   "DE - Delaware",
+                   "FL - Florida",
+                   "GA - Georgia",
+                   "GU - Guam",
+                   "HI - Hawaii",
+                   "IA - Iowa",
+                   "ID - Idaho",
+                   "IL - Illinois",
+                   "IN - Indiana",
+                   "KS - Kansas",
+                   "KY - Kentucky",
+                   "LA - Louisiana",
+                   "MA - Massachusetts",
+                   "MD - Maryland",
+                   "ME - Maine",
+                   "MI - Michigan",
+                   "MN - Minnesota",
+                   "MO - Missouri",
+                   "MS - Mississippi",
+                   "MT - Montana",
+                   "NC - North Carolina",
+                   "ND - North Dakota",
+                   "NE - Nebraska",
+                   "NH - New Hampshire",
+                   "NJ - New Jersey",
+                   "NM - New Mexico",
+                   "NV - Nevada",
+                   "NY - New York",
+                   "OH - Ohio",
+                   "OK - Oklahoma",
+                   "OR - Oregon",
+                   "PA - Pennsylvania",
+                   "PR - Puerto Rico",
+                   "RI - Rhode Island",
+                   "SC - South Carolina",
+                   "SD - South Dakota",
+                   "TN - Tennessee",
+                   "TX - Texas",
+                   "UT - Utah",
+                   "VA - Virginia",
+                   "VI - Virgin Islands",
+                   "VT - Vermont",
+                   "WA - Washington", 
+                   "WI - Wisconsin", 
+                   "WV - West Virginia", 
+                   "WY - Wyoming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +81,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+    }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ""
+    }
 }
 
