f4c4e43e97af8e066ae420795f568bc22de5f0b4
Brady Mirkes <bradymirkes@gmail.com>
2016-09-21 18:44:39 -0500

    State Picker working

--- MiraclePillsPicker/ViewController.swift
+++ MiraclePillsPicker/ViewController.swift
@@ -88,10 +88,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 0
+        return states.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return ""
+        return states[row]
     }
 }
 
